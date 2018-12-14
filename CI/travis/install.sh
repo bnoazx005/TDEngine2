e5bb67ffff1261a69a8ba361c5c583c4bfd6391f
bnoazx005 <ildar2571@yandex.ru>
2018-12-14 19:39:26 +0400

    Fix broken CI pipeline for TravisCI
    Changes:
    * libc++-dev dependency was added into install.sh.
    * The compiler's configuration in CMakelists.txt was changed. C++14
    standard was enabled for UNIX platforms.

--- CI/travis/install.sh
+++ CI/travis/install.sh
@@ -14,7 +14,7 @@ sudo apt-get update -qq
 sudo apt-get install -qq g++-7
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 90
 
-sudo apt-get install -qq build-essential xorg-dev libgl1-mesa-dev libglew-dev
+sudo apt-get install -qq build-essential xorg-dev libgl1-mesa-dev libglew-dev libc++-dev
 
 CMAKE_VERSION=3.10.1
 CMAKE_VERSION_DIR=v3.10
@@ -28,8 +28,3 @@ wget --quiet $CMAKE_URL
 mkdir -p $CMAKE_DIR
 tar --strip-components=1 -xzf $CMAKE_TAR -C $CMAKE_DIR
 export PATH=$CMAKE_DIR/bin:$PATH
-
-#install git
-sudo add-apt-repository -y ppa:git-core/ppa
-sudo apt-get update
-sudo apt-get install --only-upgrade -y git
