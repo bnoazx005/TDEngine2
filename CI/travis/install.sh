3a18b904d40c5d3bf1d75772b2084422351ae8b8
bnoazx005 <ildar2571@yandex.ru>
2018-12-19 12:46:25 +0400

    Update GLEW's version for TravisCI
    Changes:
    * install.sh script was changed. A new version of GLEW library was
    added.

--- CI/travis/install.sh
+++ CI/travis/install.sh
@@ -14,7 +14,9 @@ sudo apt-get update -qq
 sudo apt-get install -qq g++-7
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 90
 
-sudo apt-get install -qq build-essential xorg-dev libgl1-mesa-dev libglew-dev libc++-dev
+sudo apt-get install -qq unzip
+
+sudo apt-get install -qq build-essential xorg-dev libgl1-mesa-dev libglew-dev libc++-dev libxmu-dev libxi-dev
 
 CMAKE_VERSION=3.10.1
 CMAKE_VERSION_DIR=v3.10
@@ -28,3 +30,20 @@ wget --quiet $CMAKE_URL
 mkdir -p $CMAKE_DIR
 tar --strip-components=1 -xzf $CMAKE_TAR -C $CMAKE_DIR
 export PATH=$CMAKE_DIR/bin:$PATH
+
+GLEW_VERSION=2.1.0
+
+GLEW_ARCHIVE=glew-$GLEW_VERSION.zip
+GLEW_URL=https://sourceforge.net/projects/glew/files/glew/$GLEW_VERSION/$GLEW_ARCHIVE
+GLEW_DIR=$(pwd)/glew-$GLEW_VERSION
+
+wget --quiet $GLEW_URL
+mkdir -p $GLEW_DIR
+unzip $GLEW_ARCHIVE -d $GLEW_DIR
+
+#build GLEW
+cd $GLEW_DIR
+
+make
+sudo make install
+make clean
