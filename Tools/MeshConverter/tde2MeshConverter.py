e5c6cbd58c6465aa26b96d3ec84b1d92ffa51d82
bnoazx005 <ildar2571@yandex.ru>
2019-12-10 21:10:23 +0400

    Implement arguments parsing for tde2MeshConverter.py
    Changes:
    * Arguments parsing and parser selection algorithms were developed for
    mesh converter utility

--- Tools/MeshConverter/tde2MeshConverter.py
+++ Tools/MeshConverter/tde2MeshConverter.py
@@ -6,6 +6,24 @@ Author: Ildar Kasimov
 The script is used to convert DCC files (FBX, OBJ) into *.MESH internal binary format
 """
 import argparse
+import os.path
+try:
+	from FbxCommon import *
+except ImportError:
+	print("Error: module FbxCommon failed to import.\n")
+	print("Copy the files located in the compatible sub-folder lib/python<version> into your python interpreter site-packages folder.")
+	import platform
+	if platform.system() == 'Windows' or platform.system() == 'Microsoft':
+		print('For example: copy ..\\..\\lib\\Python27_x64\\* C:\\Python27\\Lib\\site-packages')
+	elif platform.system() == 'Linux':
+		print('For example: cp ../../lib/Python27_x64/* /usr/local/lib/python2.7/site-packages')
+	elif platform.system() == 'Darwin':
+		print('For example: cp ../../lib/Python27_x64/* /Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages')
+	sys.exit(1)
+
+
+FBX_TYPE = 0
+OBJ_TYPE = 1
 
 
 def parse_args():
@@ -19,9 +37,46 @@ def parse_args():
 	return parser.parse_args()
 
 
+def get_output_filename(inputFilename):
+	return os.path.splitext(inputFilename)[0] + '.mesh'
+
+
+def get_filetype_from_path(filename):
+	fileExtension =  os.path.splitext(filename)[1]
+
+	extension2fileType = {
+		'.fbx' : FBX_TYPE,
+		'.obj' : OBJ_TYPE
+	}
+
+	return extension2fileType[fileExtension.lower()]
+
+
+def read_fbx_mesh_data(inputFilename):
+	sdkManager, scene = InitializeSdkObjects()
+	lResult = LoadScene(sdkManager, scene, inputFilename)
+
+	print('read_fbx_mesh_data: %s' % inputFilename)
+	return
+
+
+def save_mesh_data(outputFilename):
+	print('save_mesh_data: %s' % outputFilename)
+	return
+
+
+FileTypeReaders = {
+	FBX_TYPE : read_fbx_mesh_data
+}
+
+
 def main():
 	args = parse_args()
-	print(args)
+	
+	fileReaderFunction = FileTypeReaders[get_filetype_from_path(args.input)]
+
+	fileReaderFunction(args.input) # first step is to read information from original file
+	save_mesh_data(args.output if args.output else get_output_filename(args.input))
 
 	return 0
 
