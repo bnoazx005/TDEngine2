7bd884eb32dc6933dd2fea3285e744c3de0988fc
bnoazx005 <ildar2571@yandex.ru>
2020-01-06 17:04:41 +0400

    Add a new class CMathUtils
    Changes:
    * A few mathematical functions were defined within CMathUtils.

--- TDEngine2/include/math/MathUtils.h
+++ TDEngine2/include/math/MathUtils.h
@@ -7,6 +7,7 @@
 #pragma once
 
 
+#include "./../utils/Config.h"
 #include "./../utils/Types.h"
 
 
@@ -27,6 +28,23 @@ namespace TDEngine2
 			constexpr static F32 Deg2Rad = Pi / 180.0f;
 
 			constexpr static F32 Rad2Deg = 180.0f / Pi;
+	};
+
+
+	/*!
+		class CMathUtils
+
+		\bruef The static class that contains common used mathematical functions
+	*/
+
+	class CMathUtils
+	{
+		public:
+			TDE2_API constexpr static inline F32 Max(F32 a, F32 b) { return (a < b) ? b : a; }
+			TDE2_API constexpr static inline F32 Min(F32 a, F32 b) { return (a < b) ? a : b; }
+			TDE2_API constexpr static inline F32 Clamp(F32 leftBound, F32 rightBound, F32 value) { return Min(rightBound, Max(leftBound, value)); }
+			TDE2_API constexpr static inline F32 Clamp01(F32 value) { return Min(1.0f, Max(0.0f, value)); }
+			TDE2_API constexpr static inline F32 Lerp(F32 a, F32 b, F32 t) { return a * (1.0f - t) + b * t; }
 	};
 
 
