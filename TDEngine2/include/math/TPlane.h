f9a22e6d577db04979a001015e6e7754e457a448
bnoazx005 <ildar2571@yandex.ru>
2020-01-28 21:39:22 +0400

    Add class which defines a plane
    Changes:
    * A new template class TPlane was defined in TPlane.h.

---
+++ TDEngine2/include/math/TPlane.h
@@ -0,0 +1,67 @@
+/*!
+	/file TPlane.h
+	/date 28.01.2020
+	/authors Kasimov Ildar
+*/
+
+#pragma once
+
+
+#include "./../utils/Types.h"
+#include "./../utils/Config.h"
+#include "./../math/TVector3.h"
+
+
+namespace TDEngine2
+{
+	/*!
+		struct TPlane
+
+		\brief The structure represents 3D plane
+	*/
+
+	template <typename T>
+	struct TPlane
+	{
+		T a, b, c, d;
+
+		TDE2_API TPlane() = default;
+		TDE2_API TPlane(const TPlane& plane) = default;
+		TDE2_API TPlane(TPlane&& plane) = default;
+
+		/*!
+			\brief The constructor which defines a plane based on its normal and distance's value
+
+			\param[in] normal A 3d unit-length vector which defines a direction and front-face of the plane
+			\param[in] distance A value on which the plane is stands off from the origin
+		*/
+
+		TDE2_API TPlane(const TVector3& normal, F32 distance):
+			a(normal.x), b(normal.y), c(normal.z), d(distance)
+		{
+		}
+
+		/*!
+			\brief The constructor defines a plane based on three points that lie on it
+
+			\param[in] points An array of three 3d points
+		*/
+
+		TDE2_API TPlane(const TVector3& points[3])
+		{
+			TVector3 e1 = points[1] - point[0];
+			TVector3 e2 = points[2] - point[0];
+
+			TVector3 n = Normalize(Cross(e2, e1));
+
+			a = n.x;
+			b = n.y;
+			c = n.z;
+			d = Dot(n, points[0]);
+		}
+	};
+
+
+	typedef TPlane<F32> TPlaneF32;
+
+}
---
+++ TDEngine2/source/math/TPlane.cpp
@@ -0,0 +1,6 @@
+#include "./../../include/math/TPlane.h"
+
+
+namespace TDEngine2
+{
+}
