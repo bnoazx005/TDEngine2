700be620c32d7215039fa1f3b498898323710f64
bnoazx005 <ildar2571@yandex.ru>
2020-11-30 20:51:09 +0400

    Add build scripts for UNIX systems

---
+++ build/prepare_build_generic_unix.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# "The script for building the engine and its toolset with CMake under UNIX platforms" 
+echo off
+
+# "Get all submodules"
+git submodule update --recursive --init
+
+if [$? -ne 0]; then
+	pause
+	goto finalize
+fi
+
+
+# "Build bullet3 first"
+
+TDE2_USE_INSTALLED_BULLET="OFF"
+
+pushd "../TDEngine2/deps/bullet3"
+	cmake -G $1 -DBUILD_SHARED_LIBS=OFF -DUSE_GRAPHICAL_BENCHMARK=OFF -DUSE_MSVC_RUNTIME_LIBRARY_DLL=ON .. && cmake --build .
+
+	if [[$TDE2_INSTALL_BULLET3]]; then
+		TDE2_USE_INSTALLED_BULLET="ON"
+		cmake install
+	fi
+popd
+
+if [$? -ne 0]; then
+	pause
+	goto finalize
+fi
+
+
+cmake -G $1 -DUSE_EXTERNAL_BULLET_LIBRARY=$TDE2_USE_INSTALLED_BULLET .. && \
+cmake --build .
+
+if [$? -ne 0]; then
+	pause
+	goto finalize
+fi
+
+:finalize
+exit $?
