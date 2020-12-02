af23d0f9a568c973ab1a79726e9115cb1e9ac362
bnoazx005 <ildar2571@yandex.ru>
2020-12-02 20:38:19 +0400

    Add script for code generation under Windows

---
+++ build/run_codegeneration.bat
@@ -0,0 +1,8 @@
+echo off
+
+set INTROSPECTOR_DIR="../TDEngine2/tools/Introspector/"
+
+set ENGINE_HEADERS_DIR="../TDEngine2/include/"
+set OUTPUT_DIR=%ENGINE_HEADERS_DIR%
+
+%INTROSPECTOR_DIR%"/tde2_introspector.exe" %ENGINE_HEADERS_DIR% -O %OUTPUT_DIR% --emit-enums --emit-classes --emit-structs --exclude-paths="/platform/unix/" --exclude-typenames="(.*?)Unix(.*?)"
