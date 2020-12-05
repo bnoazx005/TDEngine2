bf13af9bdb391fa97343eeffd777933e9a592abb
bnoazx005 <ildar2571@yandex.ru>
2020-12-05 18:03:28 +0400

    Update build preparation scripts for Windows

--- build/prepare_build_generic_win.bat
+++ build/prepare_build_generic_win.bat
@@ -1,6 +1,8 @@
-rem "The script for building the engine and its toolset with CMake and Microsoft Visual Studio" 
-rem "%1 argument is CMake generator name; %2 is extra build configuration"
 echo off
+
+rem "The script for building the engine and its toolset with CMake and Microsoft Visual Studio" 
+rem "%1 argument is CMake generator name; %2 is buildd configuration"
+
 
 rem "Get all submodules"
 git submodule update --recursive --init
@@ -15,7 +17,7 @@ rem "Build bullet3 first"
 set TDE2_USE_INSTALLED_BULLET="OFF"
 
 pushd "../TDEngine2/deps/bullet3"
-	cmake -G %1 -DBUILD_SHARED_LIBS=OFF -DUSE_GRAPHICAL_BENCHMARK=OFF -DUSE_MSVC_RUNTIME_LIBRARY_DLL=ON . && cmake --build .
+	cmake -G %1 -DBUILD_SHARED_LIBS=OFF -DUSE_GRAPHICAL_BENCHMARK=OFF -DUSE_MSVC_RUNTIME_LIBRARY_DLL=ON -DCMAKE_BUILD_TYPE=%2 . && cmake --build . --config %2
 
 	if defined TDE2_INSTALL_BULLET3 (
 		set TDE2_USE_INSTALLED_BULLET="ON"
@@ -33,8 +35,7 @@ call run_codegeneration.bat
 
 rem "Build main project"
 
-cmake -G %1 -DUSE_EXTERNAL_BULLET_LIBRARY=%TDE2_USE_INSTALLED_BULLET% .. && \
-cmake --build . %2
+cmake -G %1 -DUSE_EXTERNAL_BULLET_LIBRARY=%TDE2_USE_INSTALLED_BULLET% -DCMAKE_BUILD_TYPE=%2 .. && cmake --build . %2 . --config %2
 
 if %ERRORLEVEL% neq 0 (
 	if not defined TDE2_SKIP_ERROR_WAIT_USER_RESPONSE (
