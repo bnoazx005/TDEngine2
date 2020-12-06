38fe4c6258ba685d103d3228938201ecaf5f7ac3
bnoazx005 <ildar2571@yandex.ru>
2020-12-06 13:40:21 +0400

    Update travis' scripts

--- build/prepare_build_generic_unix.sh
+++ build/prepare_build_generic_unix.sh
@@ -17,7 +17,7 @@ fi
 TDE2_USE_INSTALLED_BULLET="OFF"
 
 pushd "../TDEngine2/deps/bullet3"
-	cmake -G $1 -DBUILD_SHARED_LIBS=OFF -DUSE_GRAPHICAL_BENCHMARK=OFF -DUSE_MSVC_RUNTIME_LIBRARY_DLL=ON . && cmake --build .
+	cmake -G $1 -DBUILD_SHARED_LIBS=OFF -DUSE_GRAPHICAL_BENCHMARK=OFF -DUSE_MSVC_RUNTIME_LIBRARY_DLL=ON -DCMAKE_BUILD_TYPE=$2 . && cmake --build . --config $2
 
 	if [[$TDE2_INSTALL_BULLET3]]; then
 		TDE2_USE_INSTALLED_BULLET="ON"
@@ -31,8 +31,10 @@ if [$? -ne 0]; then
 fi
 
 
-cmake -G $1 -DUSE_EXTERNAL_BULLET_LIBRARY=$TDE2_USE_INSTALLED_BULLET .. && \
-cmake --build .
+sh ./run_codegeneration.sh
+
+
+cmake -G $1 -DUSE_EXTERNAL_BULLET_LIBRARY=$TDE2_USE_INSTALLED_BULLET -DCMAKE_BUILD_TYPE=$2  .. && cmake --build . --config $2
 
 if [$? -ne 0]; then
 	pause
---
+++ build/run_codegeneration.sh
@@ -0,0 +1,8 @@
+echo off
+
+INTROSPECTOR_DIR="../TDEngine2/tools/Introspector/"
+
+ENGINE_HEADERS_DIR="../TDEngine2/include/"
+OUTPUT_DIR=$ENGINE_HEADERS_DIR
+
+$INTROSPECTOR_DIR"/tde2_introspector" $ENGINE_HEADERS_DIR -O $OUTPUT_DIR --emit-enums --emit-classes --emit-structs --exclude-paths="/platform/win32/" --exclude-typenames="(.*?)Win(.*?)"
