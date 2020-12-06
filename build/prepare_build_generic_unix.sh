8c67103288aed526408daa848c5633f7d2732fdd
bnoazx005 <ildar2571@yandex.ru>
2020-12-06 13:46:55 +0400

    Update scripts for Unix systems

--- build/prepare_build_generic_unix.sh
+++ build/prepare_build_generic_unix.sh
@@ -12,12 +12,15 @@ if [$? -ne 0]; then
 fi
 
 
+GENERATOR_NAME="Unix Makefiles"
+
+
 # "Build bullet3 first"
 
 TDE2_USE_INSTALLED_BULLET="OFF"
 
 pushd "../TDEngine2/deps/bullet3"
-	cmake -G $1 -DBUILD_SHARED_LIBS=OFF -DUSE_GRAPHICAL_BENCHMARK=OFF -DUSE_MSVC_RUNTIME_LIBRARY_DLL=ON -DCMAKE_BUILD_TYPE=$2 . && cmake --build . --config $2
+	cmake -G $GENERATOR_NAME  -DBUILD_SHARED_LIBS=OFF -DUSE_GRAPHICAL_BENCHMARK=OFF -DUSE_MSVC_RUNTIME_LIBRARY_DLL=ON -DCMAKE_BUILD_TYPE=$2 . && cmake --build . --config $2
 
 	if [[$TDE2_INSTALL_BULLET3]]; then
 		TDE2_USE_INSTALLED_BULLET="ON"
@@ -34,7 +37,7 @@ fi
 sh ./run_codegeneration.sh
 
 
-cmake -G $1 -DUSE_EXTERNAL_BULLET_LIBRARY=$TDE2_USE_INSTALLED_BULLET -DCMAKE_BUILD_TYPE=$2  .. && cmake --build . --config $2
+cmake -G $GENERATOR_NAME -DUSE_EXTERNAL_BULLET_LIBRARY=$TDE2_USE_INSTALLED_BULLET -DCMAKE_BUILD_TYPE=$2  .. && cmake --build . --config $2
 
 if [$? -ne 0]; then
 	pause
--- build/run_codegeneration.sh
+++ build/run_codegeneration.sh
@@ -5,4 +5,5 @@ INTROSPECTOR_DIR="../TDEngine2/tools/Introspector/"
 ENGINE_HEADERS_DIR="../TDEngine2/include/"
 OUTPUT_DIR=$ENGINE_HEADERS_DIR
 
+chmod +x $INTROSPECTOR_DIR"/tde2_introspector"
 $INTROSPECTOR_DIR"/tde2_introspector" $ENGINE_HEADERS_DIR -O $OUTPUT_DIR --emit-enums --emit-classes --emit-structs --exclude-paths="/platform/win32/" --exclude-typenames="(.*?)Win(.*?)"
