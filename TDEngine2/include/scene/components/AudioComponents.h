e8b39193c225a2b33f2d32df76f7414a2e493da5
bnoazx005 <ildar2571@yandex.ru>
2021-01-08 21:18:29 +0400

    Add a new component which is AudioSource

--- TDEngine2/include/scene/components/AudioComponents.h
+++ TDEngine2/include/scene/components/AudioComponents.h
@@ -8,9 +8,184 @@
 
 
 #include "../../ecs/CBaseComponent.h"
+#include <string>
 
 
 namespace TDEngine2
 {
 	TDE2_DECLARE_FLAG_COMPONENT(AudioListenerComponent);
+
+
+	/*!
+		struct TAudioSourceComponentParameters
+
+		\brief The structure contains parameters for creation of CAudioSourceComponent
+	*/
+
+	typedef struct TAudioSourceComponentParameters : public TBaseComponentParameters
+	{
+		std::string mAudioClipId;
+
+		bool mIsMuted = false;
+		bool mIsPaused = false;
+		bool mIsLooped = false;
+
+		F32 mVolume = 1.0f;
+		F32 mPanning = 0.0f;
+	} TAudioSourceComponentParameters;
+
+
+	/*!
+		\brief A factory function for creation objects of CAudioSourceComponent's type.
+
+		\param[out] result Contains RC_OK if everything went ok, or some other code, which describes an error
+
+		\return A pointer to CAudioSourceComponent's implementation
+	*/
+
+	TDE2_API IComponent* CreateAudioSourceComponent(E_RESULT_CODE& result);
+
+
+	/*!
+		class CAudioSourceComponent
+
+		\brief The class represents a component of a 3D audio source 
+	*/
+
+	class CAudioSourceComponent : public CBaseComponent
+	{
+		public:
+			friend TDE2_API IComponent* CreateAudioSourceComponent(E_RESULT_CODE&);
+		public:
+			TDE2_REGISTER_COMPONENT_TYPE(CAudioSourceComponent)
+
+			/*!
+				\brief The method initializes an internal state
+
+				\return RC_OK if everything went ok, or some other code, which describes an error
+			*/
+
+			TDE2_API E_RESULT_CODE Init();
+
+			/*!
+				\brief The method deserializes object's state from given reader
+
+				\param[in, out] pReader An input stream of data that contains information about the object
+
+				\return RC_OK if everything went ok, or some other code, which describes an error
+			*/
+
+			TDE2_API E_RESULT_CODE Load(IArchiveReader* pReader) override;
+
+			/*!
+				\brief The method serializes object's state into given stream
+
+				\param[in, out] pWriter An output stream of data that writes information about the object
+
+				\return RC_OK if everything went ok, or some other code, which describes an error
+			*/
+
+			TDE2_API E_RESULT_CODE Save(IArchiveWriter* pWriter) override;
+
+			TDE2_API E_RESULT_CODE SetAudioClipId(const std::string& id);
+
+			TDE2_API void SetMuted(bool value);
+			TDE2_API void SetPaused(bool value);
+			TDE2_API void SetLooped(bool value);
+
+			TDE2_API E_RESULT_CODE SetVolume(F32 value);
+			TDE2_API E_RESULT_CODE SetPanning(F32 value);
+
+			TDE2_API const std::string& GetAudioClipId(const std::string& id) const;
+
+			TDE2_API bool IsMuted(bool value) const;
+			TDE2_API bool IsPaused(bool value) const;
+			TDE2_API bool IsLooped(bool value) const;
+
+			TDE2_API F32 GetVolume(F32 value) const;
+			TDE2_API F32 GetPanning(F32 value) const;
+		protected:
+			DECLARE_INTERFACE_IMPL_PROTECTED_MEMBERS(CAudioSourceComponent)
+		protected:
+			std::string mAudioClipId;
+
+			bool mIsMuted;
+			bool mIsPaused;
+			bool mIsLooped;
+
+			F32 mVolume;
+			F32 mPanning;
+	};
+
+
+	/*!
+		\brief A factory function for creation objects of CAudioSourceComponentFactory's type.
+
+		\param[out] result Contains RC_OK if everything went ok, or some other code, which describes an error
+
+		\return A pointer to CAudioSourceComponentFactory's implementation
+	*/
+
+	TDE2_API IComponentFactory* CreateAudioSourceComponentFactory(E_RESULT_CODE& result);
+
+
+	/*!
+		class CAudioSourceComponentFactory
+
+		\brief The class is factory facility to create a new objects of CAudioSourceComponent type
+	*/
+
+	class CAudioSourceComponentFactory : public IComponentFactory, public CBaseObject
+	{
+		public:
+			friend TDE2_API IComponentFactory* CreateAudioSourceComponentFactory(E_RESULT_CODE& result);
+		public:
+			/*!
+				\brief The method initializes an internal state of a factory
+
+				\return RC_OK if everything went ok, or some other code, which describes an error
+			*/
+
+			TDE2_API E_RESULT_CODE Init();
+
+			/*!
+				\brief The method frees all memory occupied by the object
+
+				\return RC_OK if everything went ok, or some other code, which describes an error
+			*/
+
+			TDE2_API E_RESULT_CODE Free() override;
+
+			/*!
+				\brief The method creates a new instance of a component based on passed parameters
+
+				\param[in] pParams An object that contains parameters that are needed for the component's creation
+
+				\return A pointer to a new instance of IComponent type
+			*/
+
+			TDE2_API IComponent* Create(const TBaseComponentParameters* pParams) const override;
+
+			/*!
+				\brief The method creates a new instance of a component based on passed parameters
+
+				\param[in] pParams An object that contains parameters that are needed for the component's creation
+
+				\return A pointer to a new instance of IComponent type
+			*/
+
+			TDE2_API IComponent* CreateDefault(const TBaseComponentParameters& params) const override;
+
+			/*!
+				\brief The method returns an identifier of a component's type, which
+				the factory serves
+
+				\return The method returns an identifier of a component's type, which
+				the factory serves
+			*/
+
+			TDE2_API TypeId GetComponentTypeId() const override;
+		protected:
+			DECLARE_INTERFACE_IMPL_PROTECTED_MEMBERS(CAudioSourceComponentFactory)
+	};
 }
--- TDEngine2/source/scene/components/AudioComponents.cpp
+++ TDEngine2/source/scene/components/AudioComponents.cpp
@@ -4,4 +4,224 @@
 namespace TDEngine2
 {
 	TDE2_DEFINE_FLAG_COMPONENT(AudioListenerComponent);
+
+
+	CAudioSourceComponent::CAudioSourceComponent() :
+		CBaseComponent(), mVolume(1.0f), mPanning(0.0f)
+	{
+	}
+
+	E_RESULT_CODE CAudioSourceComponent::Init()
+	{
+		if (mIsInitialized)
+		{
+			return RC_FAIL;
+		}
+
+		mIsInitialized = true;
+
+		return RC_OK;
+	}
+
+	E_RESULT_CODE CAudioSourceComponent::Load(IArchiveReader* pReader)
+	{
+		if (!pReader)
+		{
+			return RC_FAIL;
+		}
+
+		mAudioClipId = pReader->GetString("clip_id");
+
+		mIsMuted = pReader->GetBool("is_muted");
+		mIsPaused = pReader->GetBool("is_paused");
+		mIsLooped = pReader->GetBool("is_looped");
+
+		mVolume = pReader->GetFloat("volume");
+		mPanning = pReader->GetFloat("panning");
+
+		return RC_OK;
+	}
+
+	E_RESULT_CODE CAudioSourceComponent::Save(IArchiveWriter* pWriter)
+	{
+		if (!pWriter)
+		{
+			return RC_FAIL;
+		}
+
+		pWriter->BeginGroup("component");
+		{
+			pWriter->SetUInt32("type_id", static_cast<U32>(CAudioSourceComponent::GetTypeId()));
+			pWriter->SetString("clip_id", mAudioClipId);
+
+			pWriter->SetBool("is_muted", mIsMuted);
+			pWriter->SetBool("is_paused", mIsPaused);
+			pWriter->SetBool("is_looped", mIsLooped);
+
+			pWriter->SetFloat("volume", mVolume);
+			pWriter->SetFloat("panning", mPanning);
+		}
+		pWriter->EndGroup();
+
+		return RC_OK;
+	}
+
+	E_RESULT_CODE CAudioSourceComponent::SetAudioClipId(const std::string& id)
+	{
+		mAudioClipId = id;
+		return RC_OK;
+	}
+
+	void CAudioSourceComponent::SetMuted(bool value)
+	{
+		mIsMuted = value;
+	}
+
+	void CAudioSourceComponent::SetPaused(bool value)
+	{
+		mIsPaused = value;
+	}
+
+	void CAudioSourceComponent::SetLooped(bool value)
+	{
+		mIsLooped = value;
+	}
+
+	E_RESULT_CODE CAudioSourceComponent::SetVolume(F32 value)
+	{
+		if (value > 1.0f || value < 0.0f)
+		{
+			return RC_INVALID_ARGS;
+		}
+
+		mVolume = value;
+
+		return RC_OK;
+	}
+
+	E_RESULT_CODE CAudioSourceComponent::SetPanning(F32 value)
+	{
+		if (value > 1.0f || value < -1.0f)
+		{
+			return RC_INVALID_ARGS;
+		}
+
+		mPanning = value;
+
+		return RC_OK;
+	}
+
+	const std::string& CAudioSourceComponent::GetAudioClipId(const std::string& id) const
+	{
+		return mAudioClipId;
+	}
+
+	bool CAudioSourceComponent::IsMuted(bool value) const
+	{
+		return mIsMuted;
+	}
+
+	bool CAudioSourceComponent::IsPaused(bool value) const
+	{
+		return mIsPaused;
+	}
+
+	bool CAudioSourceComponent::IsLooped(bool value) const
+	{
+		return mIsLooped;
+	}
+
+	F32 CAudioSourceComponent::GetVolume(F32 value) const
+	{
+		return mVolume;
+	}
+
+	F32 CAudioSourceComponent::GetPanning(F32 value) const
+	{
+		return mPanning;
+	}
+
+
+	IComponent* CreateAudioSourceComponent(E_RESULT_CODE& result)
+	{
+		return CREATE_IMPL(IComponent, CAudioSourceComponent, result);
+	}
+
+
+	CAudioSourceComponentFactory::CAudioSourceComponentFactory() :
+		CBaseObject()
+	{
+	}
+
+	E_RESULT_CODE CAudioSourceComponentFactory::Init()
+	{
+		if (mIsInitialized)
+		{
+			return RC_FAIL;
+		}
+
+		mIsInitialized = true;
+
+		return RC_OK;
+	}
+
+	E_RESULT_CODE CAudioSourceComponentFactory::Free()
+	{
+		if (!mIsInitialized)
+		{
+			return RC_FAIL;
+		}
+
+		mIsInitialized = false;
+
+		delete this;
+
+		return RC_OK;
+	}
+
+	IComponent* CAudioSourceComponentFactory::Create(const TBaseComponentParameters* pParams) const
+	{
+		if (!pParams)
+		{
+			return nullptr;
+		}
+
+		E_RESULT_CODE result = RC_OK;
+
+		CAudioSourceComponent* pAudioSource = dynamic_cast<CAudioSourceComponent*>(CreateAudioSourceComponent(result));
+		if (pAudioSource)
+		{
+			if (const TAudioSourceComponentParameters* params = static_cast<const TAudioSourceComponentParameters*>(pParams))
+			{
+				pAudioSource->SetAudioClipId(params->mAudioClipId);
+				
+				pAudioSource->SetMuted(params->mIsMuted);
+				pAudioSource->SetLooped(params->mIsLooped);
+				pAudioSource->SetPaused(params->mIsPaused);
+
+				pAudioSource->SetVolume(params->mVolume);
+				pAudioSource->SetPanning(params->mPanning);
+			}
+		}
+
+		return pAudioSource;
+	}
+
+	IComponent* CAudioSourceComponentFactory::CreateDefault(const TBaseComponentParameters& params) const
+	{
+		E_RESULT_CODE result = RC_OK;
+
+		return CreateAudioSourceComponent(result);
+	}
+
+	TypeId CAudioSourceComponentFactory::GetComponentTypeId() const
+	{
+		return CAudioSourceComponent::GetTypeId();
+	}
+
+
+	IComponentFactory* CreateAudioSourceComponentFactory(E_RESULT_CODE& result)
+	{
+		return CREATE_IMPL(IComponentFactory, CAudioSourceComponentFactory, result);
+	}
 }
