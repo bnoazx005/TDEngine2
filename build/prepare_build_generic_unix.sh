daaaeee7afd4ef02701b89de9c635b512bdff2a1
bnoazx005 <ildar2571@yandex.ru>
2021-09-24 19:51:59 +0400

    Update prepare_build_generic_unix.sh

--- build/prepare_build_generic_unix.sh
+++ build/prepare_build_generic_unix.sh
@@ -36,7 +36,7 @@ fi
 
 # try to build tde2_introspector utility
 pushd "../TDEngine2/tools/Introspector/source/"
-	cmake -G "$GENERATOR_NAME" -DCMAKE_BUILD_TYPE=$1 . && cmake --build . --config Release && cmake --install . --prefix ..
+	cmake -G "$GENERATOR_NAME" -DCMAKE_BUILD_TYPE=Release . && cmake --build . --config Release && cmake -E copy "./bin/Release/tde2_introspector" ".."
 popd
 
 if [ $? -ne 0 ]; then
