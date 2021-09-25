6a5d726deaa6a8c488e36800b6464b2587fdfc32
bnoazx005 <ildar2571@yandex.ru>
2021-09-25 20:26:25 +0400

    Update install.sh and .travis.yml

--- CI/travis/install.sh
+++ CI/travis/install.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository -y "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-10 main"
 sudo apt-get update -qq
 
-sudo apt-get install -qq g++-7
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 90
+sudo apt-get install -qq g++-9
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-9 90
 
 sudo apt-get install -qq unzip
 
-sudo apt-get install -qq build-essential xorg-dev libgl1-mesa-dev libglew-dev libc++-dev libxmu-dev libxi-dev
+sudo apt-get install -qq build-essential xorg-dev libc++-dev
+
+sudo apt-get install -qq clang-10 lld-10 libclang-common-10-dev libclang-10-dev libclang1-10 
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-10 1000
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-10 1000
 
 CMAKE_VERSION=3.10.1
 CMAKE_VERSION_DIR=v3.10
