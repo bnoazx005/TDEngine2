a984a3573104ae23aae8d5c7b1703bd02fa8b9a6
bnoazx005 <ildar2571@yandex.ru>
2023-02-09 20:23:05 +0400

    Add a prefab of UI canvas with full-screen image for auto tests

--- tests/TDE2TestsRunner/source/CUIElementsTests.cpp
+++ tests/TDE2TestsRunner/source/CUIElementsTests.cpp
@@ -49,6 +49,63 @@ TDE2_TEST_FIXTURE("UI Elements Tests")
 		}
 
 	}
+
+	TDE2_TEST_CASE("TestFullScreenImageRect_SpawnCanvasWithImage_TheImageShouldFillTheScreen")
+	{
+		static CEntity* pCanvasEntity = nullptr;
+
+		/// \note Create an entity
+		pTestCase->ExecuteAction([&]
+		{
+			IEngineCore* pEngineCore = CTestContext::Get()->GetEngineCore();
+
+			auto pSceneManager = pEngineCore->GetSubsystem<ISceneManager>();
+			auto pWorld = pSceneManager->GetWorld();
+
+			auto pMainScene = pSceneManager->GetScene(MainScene).Get();
+			TDE2_TEST_IS_TRUE(pMainScene);
+
+			pCanvasEntity = pMainScene->Spawn("TestCanvas_WithImage");
+			TDE2_TEST_IS_TRUE(pCanvasEntity);
+		});
+
+		pTestCase->WaitForNextFrame();
+
+		pTestCase->ExecuteAction([&]
+		{
+			IEngineCore* pEngineCore = CTestContext::Get()->GetEngineCore();
+
+			auto pSceneManager = pEngineCore->GetSubsystem<ISceneManager>();
+			auto pWorld = pSceneManager->GetWorld();
+
+			auto pCanvas = pCanvasEntity->GetComponent<CCanvas>();
+			
+			for (const TEntityId& currEntityId : pWorld->FindEntitiesWithComponents<CImage>())
+			{
+				CEntity* pEntity = pWorld->FindEntity(currEntityId);
+				auto pImageLayoutElement = pEntity->GetComponent<CLayoutElement>();
+				
+				const auto& worldRect = pImageLayoutElement->GetWorldRect();
+
+				TDE2_TEST_IS_TRUE(CMathUtils::Abs(worldRect.width - static_cast<F32>(pCanvas->GetWidth())) < 1e-3f);
+				TDE2_TEST_IS_TRUE(CMathUtils::Abs(worldRect.height - static_cast<F32>(pCanvas->GetHeight())) < 1e-3f);
+			}
+		});
+
+		/// \note Destroy the canvas entity
+		pTestCase->ExecuteAction([&]
+		{
+			IEngineCore* pEngineCore = CTestContext::Get()->GetEngineCore();
+
+			auto pSceneManager = pEngineCore->GetSubsystem<ISceneManager>();
+			auto pWorld = pSceneManager->GetWorld();
+
+			auto pMainScene = pSceneManager->GetScene(MainScene).Get();
+			TDE2_TEST_IS_TRUE(pMainScene);
+
+			TDE2_TEST_IS_TRUE(RC_OK == pMainScene->RemoveEntity(pCanvasEntity->GetId()));
+		});
+	}
 }
 
 #endif
