91b8c7771ac9691d4e6dcdf853eb1295b28bc287
bnoazx005 <ildar2571@yandex.ru>
2024-09-06 22:27:53 +0400

    Fix Unix CI builds

--- TDEngine2/source/editor/CStatsCounters.cpp
+++ TDEngine2/source/editor/CStatsCounters.cpp
@@ -4,10 +4,10 @@
 
 namespace TDEngine2
 {
-	std::atomic<U32> CStatsCounters::mDrawCallsCount = 0;
-	std::atomic<U32> CStatsCounters::mLoadedResourcesCount = 0;
-	std::atomic<U32> CStatsCounters::mTotalEntitiesCount = 0;
-	std::atomic<U32> CStatsCounters::mTotalComponentsCount = 0;
+	std::atomic<U32> CStatsCounters::mDrawCallsCount { 0 };
+	std::atomic<U32> CStatsCounters::mLoadedResourcesCount { 0 };
+	std::atomic<U32> CStatsCounters::mTotalEntitiesCount { 0 };
+	std::atomic<U32> CStatsCounters::mTotalComponentsCount { 0 };
 }
 
 #endif
