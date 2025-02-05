feaf3f96c8eaceffcadeff88dee48eb46cee28ed
bnoazx005 <ildar2571@yandex.ru>
2025-02-05 21:59:58 +0400

    Implement CVulkanGraphicsContext::GenerateMipMaps method

--- TDEngine2/plugins/VulkanGraphicsContext/source/CVulkanGraphicsContext.cpp
+++ TDEngine2/plugins/VulkanGraphicsContext/source/CVulkanGraphicsContext.cpp
@@ -2111,72 +2111,79 @@ namespace TDEngine2
 			return RC_OK;
 		}
 
+		BeginSectionMarker("GenerateMipMaps");
+		defer([this] { EndSectionMarker(); });
+
 		const U32 maxMipsCount = std::min(textureParams.mNumOfMipLevels, static_cast<U32>(floor(log2(std::max(textureParams.mWidth, textureParams.mHeight))) + 1));
+		const E_RESOURCE_LAYOUT originalLayout = pTextureImpl->GetLayout();
 
-		ExecuteImmediate([maxMipsCount, &textureParams, pTextureImpl](VkCommandBuffer cmdBuffer)
-			{
-				I32 mipWidth = textureParams.mWidth;
-				I32 mipHeight = textureParams.mHeight;
+		I32 mipWidth  = textureParams.mWidth;
+		I32 mipHeight = textureParams.mHeight;
 
-				const VkImageAspectFlags imageAspectFlags = E_FORMAT_TYPE::FT_D32 == textureParams.mFormat ? VK_IMAGE_ASPECT_DEPTH_BIT : VK_IMAGE_ASPECT_COLOR_BIT; // \todo Replace with CVulkanMappings::IsDepthFormat(format);
+		const VkImage currImageHandle = pTextureImpl->GetTextureHandle();
 
-				VkImageMemoryBarrier imageBarrier{};
-				imageBarrier.sType                           = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
-				imageBarrier.image                           = pTextureImpl->GetTextureHandle();
-				imageBarrier.srcQueueFamilyIndex             = VK_QUEUE_FAMILY_IGNORED;
-				imageBarrier.dstQueueFamilyIndex             = VK_QUEUE_FAMILY_IGNORED;
-				imageBarrier.subresourceRange.aspectMask = imageAspectFlags;
-				imageBarrier.subresourceRange.baseArrayLayer = 0;
-				imageBarrier.subresourceRange.layerCount     = 1;
-				imageBarrier.subresourceRange.levelCount     = 1;
+		TTextureTransitionBarrierInfo barrierInfo{};
+		barrierInfo.mCurrLayout = E_RESOURCE_LAYOUT::UNDEFINED;
+		barrierInfo.mNewLayout  = E_RESOURCE_LAYOUT::COPY_SRC;
+		barrierInfo.mHandle     = textureHandle;
+		barrierInfo.mMipLevel   = 0;
 
-				VkImage currImageHandle = pTextureImpl->GetTextureHandle();
+		TransitionBarrier(barrierInfo);
+		FlushBarriers();
 
-				for (I32 i = 1; i < static_cast<I32>(maxMipsCount); ++i)
-				{
-					imageBarrier.subresourceRange.baseMipLevel = static_cast<U32>(i - 1);
-					imageBarrier.oldLayout                     = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
-					imageBarrier.newLayout                     = VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL;
-					imageBarrier.srcAccessMask                 = VK_ACCESS_TRANSFER_WRITE_BIT;
-					imageBarrier.dstAccessMask                 = VK_ACCESS_TRANSFER_READ_BIT;
+		for (I32 i = 1; i < static_cast<I32>(maxMipsCount); ++i)
+		{
+			VkImageBlit imageBlit{};
 
-					vkCmdPipelineBarrier(cmdBuffer, VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT, 0, 0, nullptr, 0, nullptr, 1, &imageBarrier);
+			// Source
+			imageBlit.srcSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			imageBlit.srcSubresource.layerCount = 1;
+			imageBlit.srcSubresource.mipLevel   = i - 1;
+			imageBlit.srcOffsets[1].x           = static_cast<I32>(textureParams.mWidth >> (i - 1));
+			imageBlit.srcOffsets[1].y           = static_cast<I32>(textureParams.mHeight >> (i - 1));
+			imageBlit.srcOffsets[1].z           = 1;
 
-					VkImageBlit blit{};
-					blit.srcOffsets[0]                 = { 0, 0, 0 };
-					blit.srcOffsets[1]                 = { mipWidth, mipHeight, 1 };
-					blit.srcSubresource.aspectMask     = imageAspectFlags;
-					blit.srcSubresource.mipLevel       = static_cast<U32>(i - 1);
-					blit.srcSubresource.baseArrayLayer = 0;
-					blit.srcSubresource.layerCount     = 1;
-					blit.dstOffsets[0]                 = { 0, 0, 0 };
-					blit.dstOffsets[1]                 = { mipWidth > 1 ? mipWidth >> 1 : 1, mipHeight > 1 ? mipHeight >> 1 : 1, 1 };
-					blit.dstSubresource.aspectMask     = imageAspectFlags;
-					blit.dstSubresource.mipLevel       = static_cast<U32>(i);
-					blit.dstSubresource.baseArrayLayer = 0;
-					blit.dstSubresource.layerCount     = 1;
+			// Destination
+			imageBlit.dstSubresource.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			imageBlit.dstSubresource.layerCount = 1;
+			imageBlit.dstSubresource.mipLevel   = i;
+			imageBlit.dstOffsets[1].x           = static_cast<I32>(textureParams.mWidth >> i);
+			imageBlit.dstOffsets[1].y           = static_cast<I32>(textureParams.mHeight >> i);
+			imageBlit.dstOffsets[1].z           = 1;
 
-					vkCmdBlitImage(cmdBuffer, currImageHandle, VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL, currImageHandle, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 1, &blit, VK_FILTER_LINEAR);
+			barrierInfo.mMipLevel = static_cast<U32>(i);
+			barrierInfo.mCurrLayout = E_RESOURCE_LAYOUT::UNDEFINED;
+			barrierInfo.mNewLayout = E_RESOURCE_LAYOUT::COPY_DEST;
 
-					imageBarrier.oldLayout     = VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL;
-					imageBarrier.newLayout     = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-					imageBarrier.srcAccessMask = VK_ACCESS_TRANSFER_READ_BIT;
-					imageBarrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+			TransitionBarrier(barrierInfo);
+			FlushBarriers();
 
-					vkCmdPipelineBarrier(cmdBuffer, VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT, 0, 0, nullptr, 0, nullptr, 1, &imageBarrier);
+			vkCmdBlitImage(
+				_getCurrCommandBufferHandle(),
+				currImageHandle,
+				VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
+				currImageHandle,
+				VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+				1,
+				&imageBlit,
+				VK_FILTER_LINEAR);
 
-					mipWidth  = (mipWidth > 1) ? mipWidth >> 1 : 1;
-					mipHeight = (mipHeight > 1) ? mipHeight >> 1 : 1;
-				}
+			barrierInfo.mCurrLayout = E_RESOURCE_LAYOUT::COPY_DEST;
+			barrierInfo.mNewLayout = E_RESOURCE_LAYOUT::COPY_SRC;
 
-				imageBarrier.subresourceRange.baseMipLevel = maxMipsCount - 1;
-				imageBarrier.oldLayout                     = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
-				imageBarrier.newLayout                     = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
-				imageBarrier.srcAccessMask                 = VK_ACCESS_TRANSFER_WRITE_BIT;
-				imageBarrier.dstAccessMask                 = VK_ACCESS_SHADER_READ_BIT;
+			TransitionBarrier(barrierInfo);
+			FlushBarriers();
 
-				vkCmdPipelineBarrier(cmdBuffer, VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT, 0, 0, nullptr, 0, nullptr, 1, &imageBarrier);
-			});
+			mipWidth  = (mipWidth > 1) ? mipWidth >> 1 : 1;
+			mipHeight = (mipHeight > 1) ? mipHeight >> 1 : 1;
+		}
+
+		barrierInfo.mCurrLayout = E_RESOURCE_LAYOUT::COPY_SRC;
+		barrierInfo.mNewLayout  = originalLayout;
+		barrierInfo.mMipLevel   = std::nullopt;
+
+		TransitionBarrier(barrierInfo);
+		FlushBarriers();
 
 		return RC_OK;
 	}
