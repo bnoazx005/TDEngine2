df8ca816b179e01a884ea96268e465fdc78ac576
bnoazx005 <ildar2571@yandex.ru>
2026-06-29 19:59:24 +0400

    Update codegeneration scripts

--- build/run_codegeneration.bat
+++ build/run_codegeneration.bat
@@ -2,7 +2,7 @@ echo off
 
 set INTROSPECTOR_DIR="../TDEngine2/tools/Introspector/"
 
-set ENGINE_HEADERS_DIR="../TDEngine2/include/"
+set ENGINE_HEADERS_DIR="../TDEngine2/include/" "..\TDEngine2\plugins\*\include\*"
 set OUTPUT_DIR=%ENGINE_HEADERS_DIR%
 
-%INTROSPECTOR_DIR%"/tde2_introspector.exe" %ENGINE_HEADERS_DIR% -O %OUTPUT_DIR% --force --tagged-only --emit-enums --emit-classes --emit-structs --exclude-paths="/platform/unix/" --exclude-typenames="(.*?)Unix(.*?)"
+%INTROSPECTOR_DIR%"/tde2_introspector.exe" %ENGINE_HEADERS_DIR% -O %OUTPUT_DIR% --force --tagged-only --emit-enums --emit-classes --emit-structs --exclude-paths="/platform/unix/;/deps/" --exclude-typenames="(.*?)Unix(.*?)"
--- build/run_codegeneration.sh
+++ build/run_codegeneration.sh
@@ -2,8 +2,7 @@ echo off
 
 INTROSPECTOR_DIR="../TDEngine2/tools/Introspector/"
 
-ENGINE_HEADERS_DIR="../TDEngine2/include/"
-OUTPUT_DIR=$ENGINE_HEADERS_DIR
+OUTPUT_DIR="../TDEngine2/include/"
 
 chmod +x $INTROSPECTOR_DIR"/tde2_introspector"
-$INTROSPECTOR_DIR"/tde2_introspector" $ENGINE_HEADERS_DIR -O $OUTPUT_DIR --force --tagged-only  --emit-enums --emit-classes --emit-structs --exclude-paths="/platform/win32/" --exclude-typenames="(.*?)(Win|D3D11)(.*?)"
+$INTROSPECTOR_DIR"/tde2_introspector" "../TDEngine2/include/" "../TDEngine2/plugins/*/include/*" -O $OUTPUT_DIR --force --tagged-only  --emit-enums --emit-classes --emit-structs --exclude-paths="/platform/win32/;/deps/" --exclude-typenames="(.*?)(Win|D3D11)(.*?)"
